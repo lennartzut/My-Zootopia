88efb5379feac9778d05336ffb475fe9b1dcf714
Leo-Zeneo <78357307+Leo-Zeneo@users.noreply.github.com>
2024-06-14 13:54:44 +0200

    Improve card item html

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -9,26 +9,28 @@ def load_data(file_path):
 animals_data = load_data("animals_data.json")
 
 
-def fox_data(data):
-    """Prints Name, Location, Diet and Type of every fox"""
-    fox_data_string = ""
-    for fox in data:
-        fox_name = fox.get("name")
-        fox_location = fox.get("locations")[0]
-        fox_diet = fox.get("characteristics", {}).get("diet")
-        fox_type = fox.get("characteristics", {}).get("type")
-        fox_data_string += "<li class=\"cards__item\">"
-        if fox_name is not None:
-            fox_data_string += f"Name: {fox_name}<br/>\n"
-        if fox_diet is not None:
-            fox_data_string += f"Diet: {fox_diet}<br/>\n"
-        if fox_location is not None:
-            fox_data_string += f"Location: {fox_location}<br/>\n"
-        if fox_type is not None:
-            fox_data_string += f"Type: {fox_type}<br/>\n"
-        fox_data_string += "</li>"
-        fox_data_string += "\n"
-    return(fox_data_string)
+def animal_data(data):
+    """Prints Name, Location, Diet and Type of every animal"""
+    animal_data_string = ""
+    for animal in data:
+        animal_name = animal.get("name")
+        animal_location = animal.get("locations")[0]
+        animal_diet = animal.get("characteristics", {}).get("diet")
+        animal_type = animal.get("characteristics", {}).get("type")
+        animal_data_string += "<li class=\"cards__item\">\n"
+        if animal_name is not None:
+            animal_data_string += f"\t<div class=\"card__title\">{animal_name}</div>\n"
+        animal_data_string += "\t<p class=\"card__text\">\n"
+        if animal_diet is not None:
+            animal_data_string += f"\t\t<strong>Diet:</strong> {animal_diet}<br/>\n"
+        if animal_location is not None:
+            animal_data_string += f"\t\t<strong>Location:</strong> {animal_location}<br/>\n"
+        if animal_type is not None:
+            animal_data_string += f"\t\t<strong>Type:</strong> {animal_type}<br/>\n"
+        animal_data_string += "\t</p>\n"
+        animal_data_string += "</li>"
+        animal_data_string += "\n"
+    return(animal_data_string)
 
 
 # Reads local HTML file
@@ -37,7 +39,7 @@ with open("animals_template.html", "r") as file:
 
 
 # Replace text in HTML with data string
-html_new_content = html_content.replace("__REPLACE_ANIMALS_INFO__", fox_data(animals_data))
+html_new_content = html_content.replace("__REPLACE_ANIMALS_INFO__", animal_data(animals_data))
 
 
 # Write new HTML file
