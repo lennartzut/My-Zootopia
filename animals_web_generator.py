9b6d40bb7f6b5c91a6341da23984ebefb3276a68
Leo-Zeneo <78357307+Leo-Zeneo@users.noreply.github.com>
2024-06-14 13:21:40 +0200

    Bring animal data into html

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -11,19 +11,33 @@ animals_data = load_data("animals_data.json")
 
 def fox_data(data):
     """Prints Name, Location, Diet and Type of every fox"""
+    fox_data_string = ""
     for fox in data:
         fox_name = fox.get("name")
         fox_location = fox.get("locations")[0]
         fox_diet = fox.get("characteristics", {}).get("diet")
         fox_type = fox.get("characteristics", {}).get("type")
         if fox_name is not None:
-            print(f"Name: {fox_name}")
+            fox_data_string += f"Name: {fox_name}\n"
         if fox_diet is not None:
-            print(f"Diet: {fox_diet}")
+            fox_data_string += f"Diet: {fox_diet}\n"
         if fox_location is not None:
-            print(f"Location: {fox_location}")
+            fox_data_string += f"Location: {fox_location}\n"
         if fox_type is not None:
-            print(f"Type: {fox_type}")
-        print()
+            fox_data_string += f"Type: {fox_type}\n"
+        fox_data_string += "\n"
+    return(fox_data_string)
 
-fox_data(animals_data)
+
+# Reads local HTML file
+with open("animals_template.html", "r") as file:
+    html_content = file.read()
+
+
+# Replace text in HTML with data string
+html_new_content = html_content.replace("__REPLACE_ANIMALS_INFO__", fox_data(animals_data))
+
+
+# Write new HTML file
+with open("animals.html", "w") as file:
+    file.write(html_new_content)
